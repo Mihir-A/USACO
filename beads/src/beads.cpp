438d28155a34c13fc44f0754209a310de7a01629
Mihir Ananthateerta <mananthateerta063@student.fuhsd.org>
2021-12-21 14:13:40 -0800

    done with beads

--- beads/src/beads.cpp
+++ beads/src/beads.cpp
@@ -1,7 +1,56 @@
+/*
+ID: mihiran1
+LANG: C++
+TASK: beads
+*/
 #include <iostream>
+#include <fstream>
+#include <string>
+
+int breakNecklace(std::string& necklace, int length) {
+
+    int max = 0;
+    for (int pos = 0; pos < length; pos++) {
+        char letter = necklace[pos];
+        int beads = 0;
+        int j = pos;
+
+        int t;
+        if (letter != 'w') {
+            t = 1;
+        }
+        else {
+            t = 0;
+        }
+        while (t <= 2) {
+            while (j < length + pos) {
+                if ((necklace[j] == letter || necklace[j] == 'w')) {
+                    beads++;
+                    j++;
+                }
+                else {
+                    break;
+                }
+            }
+            letter = necklace[j];
+            t++;
+        }
+
+        max = std::max(max, beads);
+    }
+    return max;
+}
 
 int main() {
-	std::cout << "Hello World" << "\n";
+    std::ofstream outfile("beads.out");
+    std::ifstream infile("beads.in");
 
-	return 0;
+    int length;
+    std::string necklace;
+
+    infile >> length >> necklace;
+    necklace += necklace;
+
+    outfile << breakNecklace(necklace, length) << "\n";
+    return 0;
 }

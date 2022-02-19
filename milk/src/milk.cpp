b1f15f4bd80f60a02df9e1ec2acdb52998571654
Mihir Ananthateerta <mananthateerta063@student.fuhsd.org>
2022-02-19 09:20:14 -0800

    done with milk

--- milk/src/milk.cpp
+++ milk/src/milk.cpp
@@ -1,4 +1,8 @@
-#include <iostream>
+/*
+ID: mihiran1
+LANG: C++
+TASK: milk
+*/
 #include <fstream>
 #include <sstream>
 #include <string>
@@ -7,10 +11,28 @@
 class Farmer
 {
 public:
-	int maxAmo;
-	int price;
-	int AmoBought;
+	int maxAmo = 0;
+	int price = 0;
+	bool boughtAll = false;
 };
+
+int findPrices(std::vector<Farmer>& farmers, const int ammOfMilk) {
+	int bought = 0;
+	int price = 0;
+
+	for (int i = 0; bought < ammOfMilk; i ++) {
+		if (farmers[i].maxAmo + bought > ammOfMilk) {
+			price += farmers[i].price * (ammOfMilk - bought);
+			bought = ammOfMilk;
+		}
+		else {
+			price += farmers[i].price * farmers[i].maxAmo;
+			bought += farmers[i].maxAmo;
+			farmers[i].boughtAll = true;
+		}
+	}
+	return price;
+}
 
 void sortFarm(std::vector<Farmer>& farmers) {
 	bool sorted = false;
@@ -51,7 +73,15 @@ int main() {
 		farmers[i].maxAmo = a;
 	}
 
-	sortFarm(farmers);
+	std::ofstream fout("milk.out");
+	if (farmers.size() == 0) {
+		fout << '0' << '\n';
+	}
+	else {
+		sortFarm(farmers);
+		fout << findPrices(farmers, ammOfMilk) << '\n';
+		fout.close();
+	}
 
 	return 0;
 }

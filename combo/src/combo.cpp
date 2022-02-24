1332a850b570baebcf6709f0c23e0e38a69def49
Mihir Ananthateerta <mananthateerta063@student.fuhsd.org>
2022-02-23 16:58:53 -0800

    Done with combo

--- combo/src/combo.cpp
+++ combo/src/combo.cpp
@@ -3,48 +3,18 @@ ID: mihiran1
 LANG: C++
 TASK: combo
 */
-#include <iostream>
 #include <fstream>
 #include <vector>
 #include <array>
-#include <algorithm>
 
 int dialNum;
 
-template<size_t size, class type>
-std::ostream& operator<<(std::ostream & stream, std::array<type, size>& arr) {
-	for (auto &a : arr) {
-		stream << a << ", ";
-	}
-	stream << "\n";
-	return stream;
-}
-
-template< class type>
-std::ostream& operator<<(std::ostream& stream, std::vector<type>& arr) {
-	for (auto& a : arr) {
-		stream << a << "\n";
-	}
-	stream << "\n";
-	return stream;
-}
-
-//void printarr(std::array<int, 3>& arr) {
-//	for (int a : arr) {
-//		std::cout << a << ", ";
-//	}
-//	std::cout << "\n";
-//}
-
 int wrap(int num) {
-	if (dialNum == 1) {
-		return 1;
-	}
 	if (num < 1) {
-		return dialNum + num;
+		return wrap(dialNum + num);
 	}
 	else if (num > dialNum) {
-		return num - dialNum;
+		return wrap(num - dialNum);
 	}
 	return num;
 }
@@ -60,52 +30,32 @@ void pushBackCheck(std::vector<std::array<int, 3>>& vec, std::array<int, 3>& arr
 
 int findCombos(const std::array<int, 3>& farmerCombo, const std::array<int, 3>& masterCombo) {
 	std::vector<std::array<int, 3>> combos;
+	std::array<int, 3> combo;
 	combos.reserve(200);
-	//const std::array<int, 3> mins = { wrap(farmerCombo[0] - 2), wrap(farmerCombo[1] - 2), wrap(farmerCombo[2] - 2) };
-	std::array<int, 3> combo({farmerCombo[0] - 2,farmerCombo[1] - 2,farmerCombo[2] - 2 });
-
-	bool done = false;
-	int maxPos = 0;
-	while (!done) {
-		combos.push_back(combo);
-		combo[0] = wrap(combo[0] + 1);
-
-		for (int j = 0; j <= maxPos; j++) {
-			if (combo[j] == wrap(farmerCombo[j] + 3)) {
-				if (j == 2) {
-					done = true;
-					break;
-				}
-				combo[j] = wrap(farmerCombo[j] - 2);
-				combo[j + 1] = wrap(combo[j + 1] + 1);
-				maxPos = j + 1;
-			}
-		}
-	}
 	
-
-	done = false;
-	maxPos = 0;
-	combo = { masterCombo[0] - 2,masterCombo[1] - 2,masterCombo[2] - 2 };
-	while (!done) {
-		pushBackCheck(combos, combo);
-		combo[0] = wrap(combo[0] + 1);
-
-		for (int j = 0; j <= maxPos; j++) {
-			if (combo[j] == wrap(masterCombo[j] + 3)) {
-				if (j == 2) {
-					done = true;
-					break;
-				}
-				combo[j] = combo[j] = wrap(masterCombo[j] - 2);
-				combo[j + 1] = wrap(combo[j + 1] + 1);
-				maxPos = j + 1;
+	for (int i = farmerCombo[0] - 2; i <= farmerCombo[0] + 2; i++) {
+		for (int j = farmerCombo[1] - 2; j <= farmerCombo[1] + 2; j++) {
+			for (int w = farmerCombo[2] - 2; w <= farmerCombo[2] + 2; w++) {
+				combo[0] = wrap(i);
+				combo[1] = wrap(j);
+				combo[2] = wrap(w);
+				pushBackCheck(combos, combo);
 			}
 		}
 	}
-	std::sort(combos.begin(), combos.end());
-	std::cout << combos.size() << "\n\n\n" << combos;
-	return combos.size();
+
+	for (int i = masterCombo[0] - 2; i <= masterCombo[0] + 2; i++) {
+		for (int j = masterCombo[1] - 2; j <= masterCombo[1] + 2; j++) {
+			for (int w = masterCombo[2] - 2; w <= masterCombo[2] + 2; w++) {
+				combo[0] = wrap(i);
+				combo[1] = wrap(j);
+				combo[2] = wrap(w);
+				pushBackCheck(combos, combo);
+			}
+		}
+	}
+
+	return (int)combos.size();
 }
 
 int main() {
